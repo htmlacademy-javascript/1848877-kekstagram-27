7efba053beb29e03d58678173b367312454b8f80
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2022-09-30 20:13:05 +0700

    Добавлен вызов функции

--- js/main.js
+++ js/main.js
@@ -12,11 +12,12 @@ if (min < 0 || max < 0){
 const RandomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
 return RandomNumber;
 };
+getRandomNumber (1, 10)
 const getCheckStringLength = function (stringToTest , maxLength){
   if ( stringToTest > maxLength || stringToTest.length > maxLength) {
     return false};
   return true;
 }
 
-
+getCheckStringLength ('House', 10)
 

fdd572f26eb38d99f798580fe57c4d3fff11306c
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2022-09-30 20:40:33 +0700

    обавление пробелов

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-const getRandomNumber = function (minNumber, maxNumber) {
-let min = Math.ceil(minNumber);
-let max = Math.floor(maxNumber);
-if (max < min) {
-  let Number = min;
-  min = max;
-  max = Number;
+const getRandomNumber = function (minNumber , maxNumber) {
+let min = Math.ceil( minNumber ) ;
+let max = Math.floor( maxNumber ) ;
+if ( max < min ) {
+  let Number = min ;
+  min = max ;
+  max = Number ;
 }
-if (min < 0 || max < 0){
-  return NaN;
+if ( min < 0 || max < 0 ){
+  return NaN ;
 }
-let RandomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
-return RandomNumber;
+let RandomNumber = Math.floor ( Math.random() * ( max - min + 1 ) ) + min ;
+return RandomNumber ;
 };
-getRandomNumber (1, 10)
-const getCheckStringLength = function (stringToTest , maxLength){
-  if ( stringToTest > maxLength || stringToTest.length > maxLength) {
-    return false};
-  return true;
+getRandomNumber ( 1 , 10 )
+const getCheckStringLength = function ( stringToTest , maxLength ) {
+  if ( stringToTest > maxLength || stringToTest.length > maxLength ) {
+    return false } ;
+  return true ;
 }
-getCheckStringLength ('House', 10)
+getCheckStringLength ( 'House' , 10 )
 

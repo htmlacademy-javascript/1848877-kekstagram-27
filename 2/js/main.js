9e2f2051ccf5238fa1affe5fed9d2abeddbb4f75
Keks <keks@htmlacademy.ru>
2022-09-30 16:57:57 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,22 @@
+const getRandomNumber = function (minNumber , maxNumber) {
+  let min = Math.ceil( minNumber ) ;
+  let max = Math.floor( maxNumber ) ;
+  if ( max < min ) {
+    const Number = min ;
+    min = max ;
+    max = Number ;
+  }
+  if ( min < 0 || max < 0 ){
+    return NaN ;
+  }
+  const RandomNumber = Math.floor ( Math.random() * ( max - min + 1 ) ) + min ;
+  return RandomNumber ;
+};
+getRandomNumber ( 1 , 10 );
+const getCheckStringLength = function ( stringToTest , maxLength ) {
+  if ( stringToTest > maxLength || stringToTest.length > maxLength ) {
+    return false; }
+  return true ;
+};
+getCheckStringLength ( 'House' , 10 );
+

9d7e23011ab3510d914dd6605dbc54ba5697c6b2
Keks <keks@htmlacademy.ru>
2022-10-01 04:54:16 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,22 +1,16 @@
-const getRandomNumber = function (minNumber , maxNumber) {
-  let min = Math.ceil( minNumber ) ;
-  let max = Math.floor( maxNumber ) ;
-  if ( max < min ) {
-    const Number = min ;
-    min = max ;
-    max = Number ;
+const getRandomNumber = (minNumber, maxNumber) => {
+  let min = Math.ceil(minNumber);
+  let max = Math.floor(maxNumber);
+  if (max < min) {
+    const number = min;
+    min = max;
+    max = number;
   }
-  if ( min < 0 || max < 0 ){
-    return NaN ;
+  if (min < 0 || max < 0) {
+    return NaN;
   }
-  const RandomNumber = Math.floor ( Math.random() * ( max - min + 1 ) ) + min ;
-  return RandomNumber ;
+  return Math.floor (Math.random() * (max - min + 1)) + min;
 };
-getRandomNumber ( 1 , 10 );
-const getCheckStringLength = function ( stringToTest , maxLength ) {
-  if ( stringToTest > maxLength || stringToTest.length > maxLength ) {
-    return false; }
-  return true ;
-};
-getCheckStringLength ( 'House' , 10 );
-
+getRandomNumber (1, 10);
+const getCheckStringLength = (stringToTest , maxLength) => (stringToTest.length < maxLength);
+getCheckStringLength ('House', 10);

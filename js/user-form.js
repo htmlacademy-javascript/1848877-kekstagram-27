4d400776eb8670721039d6b82ef73ed1a5b5aa38
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2022-10-26 20:23:04 +0700

    Изменения

--- js/user-form.js
+++ js/user-form.js
@@ -1,4 +1,5 @@
 import {isMaxLength} from './data.js';
+
 const uploadFile = document.querySelector('#upload-file');
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
@@ -7,34 +8,47 @@ const imgUploadForm = document.querySelector('.img-upload__form');
 //const submitButton = document.querySelector('.img-upload__submit');
 const commentsField = document.querySelector('.text__description');
 const hashtagField = document.querySelector('.text__hashtags');
-
+//imgUploadOverlay.classList.remove('hidden');
 
 const MAX_HASHTAGS = 5;
-const MAX_LENGTH = 140;
+const MAX_LENGTH_COMMENT = 140;
 
 //создание массива хештегов
 const createHashtagArray = (value) => {
-  value.trim().toLowerCase().split(' ');
+  value.trim().toLowerCase().split(' ').sort().join('');
 };
 //проверка на валидность хештега
-/*const isValidHashtag = (value) => {
+const isValidHashtag = (value) => {
   const hashtag = createHashtagArray(value);
   const registr = /^#[a-zа-яё0-9]{1, 19}$/i;
-  const isValid = (value) => registr.test(value);
+  const isValid = (test) => registr.test(test);
   return hashtag.every(isValid);
-};*/
+};
+
 // не более 5 хештегов
 const getNumberOfHashtags = (value) => {
   const hashtags = createHashtagArray(value);
   return hashtags.length <= MAX_HASHTAGS;
 };
 //один и тот же хэш-тег не может быть использован дважды
-const getUniqeHashtag = (value) => {
+/*const getUniqeHashtag = (value) => {
   const hashtag = createHashtagArray(value);
   return new Set(hashtag).size === hashtag.length;
+};*/
+//один и тот же хэш-тег не может быть использован дважды
+const getUniqeHashtag = (value) => {
+  const hashtag = createHashtagArray(value);
+  const newHashtag = new Map ();
+  for (const hastags of value) {
+    const sorted = value.toLowerCase().split('').sort().join('');
+    newHashtag.set(sorted, hastags);
+  }
+  return Array.from(newHashtag.values(hashtag));
 };
+
+
 //длина комментария не больше 140
-const getLengthComment = (value) => isMaxLength(value, MAX_LENGTH);
+const getLengthComment = (value) => isMaxLength(value, MAX_LENGTH_COMMENT);
 
 const pristine = new Pristine(imgUploadForm, {
   classTo: 'img-upload__text',
@@ -47,8 +61,8 @@ const pristine = new Pristine(imgUploadForm, {
 
 pristine.addValidator(hashtagField, getUniqeHashtag,'один и тот же хэш-тег не может быть использован дважды');
 pristine.addValidator(hashtagField, getNumberOfHashtags,'нельзя указать больше пяти хэш-тегов');
-//pristine.addValidator(hashtagField, isValidHashtag,'Хэштег должен начинаться с "#", содержать буквы, числа (не более 20 символов, включая #)');
-pristine.addValidator(commentsField, getLengthComment, `Не более ${MAX_LENGTH} символов`);
+pristine.addValidator(hashtagField, isValidHashtag,'Хэштег должен начинаться с "#", содержать буквы, числа (не более 20 символов, включая #)');
+pristine.addValidator(commentsField, getLengthComment, `Не более ${MAX_LENGTH_COMMENT} символов`);
 
 imgUploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
